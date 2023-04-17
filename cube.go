963c8e7ef7a8bd3f74a1620d6bba662b58d4d15e
Yuquan1210 <xu1210@purdue.edu>
2023-04-17 19:04:15 -0400

    change curve to bls12_381

--- cube.go
+++ cube.go
@@ -36,14 +36,14 @@ func (circuit *CubicCircuit) Define(api frontend.API) error {
 func main() {
 	// compiles our circuit into a R1CS
 	var circuit CubicCircuit
-	ccs, _ := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
+	ccs, _ := frontend.Compile(ecc.BLS12_381.ScalarField(), r1cs.NewBuilder, &circuit)
 
 	// groth16 zkSNARK: Setup
 	pk, vk, _ := groth16.Setup(ccs)
 
 	// witness definition
 	assignment := CubicCircuit{X: 3, Sym_1: 9, Y: 27, Sym_2: 30, Out: 35}
-	witness, _ := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
+	witness, _ := frontend.NewWitness(&assignment, ecc.BLS12_381.ScalarField())
 	publicWitness, _ := witness.Public()
 
 	// groth16: Prove & Verify
